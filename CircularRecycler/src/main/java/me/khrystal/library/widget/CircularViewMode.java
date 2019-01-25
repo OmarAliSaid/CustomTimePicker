ace97390626fdfb0bba7b1b28b3fcc7e71a1be28
OmarAliSaid <o.ali@nasaq.tech>
2019-01-26 00:21:50 +0200

    removing un-necessary code from library

--- CircularRecycler/src/main/java/me/khrystal/library/widget/CircularViewMode.java
+++ CircularRecycler/src/main/java/me/khrystal/library/widget/CircularViewMode.java
@@ -14,33 +14,15 @@ import android.view.View;
  */
 public class CircularViewMode implements ItemViewMode {
 
-    private int mCircleOffset = 500;
-    private float mDegToRad = 1.0f / 180.0f * (float) Math.PI;
     private float mScalingRatio = 0.001f;
-    private float mTranslationRatio = 0.09f;
 
     public CircularViewMode() {}
-
-    public CircularViewMode(int circleOffset, float degToRad, float scalingRatio, float translationRatio) {
-        mCircleOffset = circleOffset;
-        mDegToRad = degToRad;
-        mScalingRatio = scalingRatio;
-        mTranslationRatio = translationRatio;
-
-    }
-
 
     @Override
     public void applyToView(View v, RecyclerView parent) {
         float halfHeight = v.getHeight() * 0.5f;
         float parentHalfHeight = parent.getHeight() * 0.5f;
         float y = v.getY();
-        float rot = parentHalfHeight - halfHeight - y;
-
-//        ViewCompat.setPivotX(v, 0.0f);
-//        ViewCompat.setPivotY(v, halfHeight);
-//        ViewCompat.setRotation(v, rot * 0.05f);
-//        ViewCompat.setTranslationX(v, (float)(-Math.cos(rot * mTranslationRatio * mDegToRad) + 1) * mCircleOffset);
 
         float scale = 1.0f - Math.abs(parentHalfHeight - halfHeight - y) * mScalingRatio;
         ViewCompat.setScaleX(v, scale);
